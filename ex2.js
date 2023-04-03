c21294128f1a361483dd068b21fa2512e554c395
Iris D <iris.dijkstra@hotmail.com>
2023-04-03 14:14:48 +0100

    hoisting

---
+++ ex2.js
@@ -0,0 +1,15 @@
+// Hoisting
+
+function bigBrother(){
+  function littleBrother() {
+    return 'it is me!';
+  }
+  return littleBrother();
+  function littleBrother() {
+    return 'no me!';
+  }
+}
+
+// Before running this code, what do you think the output is?
+// no, me!
+bigBrother();
