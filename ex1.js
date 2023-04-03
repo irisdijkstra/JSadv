41f9723ec81c1f1d619e869c79f7bd8cdeed0a69
Iris D <iris.dijkstra@hotmail.com>
2023-04-03 12:38:34 +0100

    solved

--- ex1.js
+++ ex1.js
@@ -2,16 +2,18 @@
 // How can you fix this code by still keeping the recursive call around, but only adding
 // something that you have learned in this section to not cause this stack overflow.
 
-//fill array with 60000 elements
 const list = new Array(60000).join('1.1').split('.');
-console.log(list)
 
 function removeItemsFromList() {
-    var item = list.pop();
+  setTimeout(testloop, 0)
+
+  function testloop() {
+    let item = list.pop();
 
     if (item) {
-        removeItemsFromList();
-    }
+      removeItemsFromList();
+    };
+  };
 };
 
 removeItemsFromList();
