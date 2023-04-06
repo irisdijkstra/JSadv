2a3aaaf18105788950497e104364d81de9caaee1
Iris D <iris.dijkstra@hotmail.com>
2023-04-06 11:25:16 +0100

    type coersion

---
+++ ex7.js
@@ -0,0 +1,27 @@
+// Type coersion
+false == ""
+console.log(false == "")
+
+false == []
+console.log(false == [])
+
+false == {}
+console.log(false == {})
+
+"" == 0
+console.log("" == 0)
+
+"" == []
+console.log("" == [])
+
+"" == {}
+console.log("" == {})
+
+0 == []
+console.log(0 == [])
+
+0 == {}
+console.log(0 == {})
+
+0 == null
+console.log(0 == null)
