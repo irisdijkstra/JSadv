fb2eb1dc7ddcaedaeda35b19e0496c471659711c
Iris D <iris.dijkstra@hotmail.com>
2023-04-11 17:12:44 +0200

    modified as prototype functions

---
+++ ex10.js
@@ -0,0 +1,4 @@
+// How would you be able to create your own .bind() method using call or apply?
+
+Function.prototype.bind = function(){
+}
--- ex9.js
+++ ex9.js
@@ -4,14 +4,22 @@
 //Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
 // Result = 1899
 
-const dateObject = new Date('1900-10-10');
-dateObject.lastYear = () => dateObject.getFullYear()-1;
+Date.prototype.lastYear = function () {
+   return this.getFullYear()-1;
+};
 
-console.log(dateObject.lastYear());
+console.log(new Date('1900-10-10').lastYear());
 
 //#Bonus
 // Mofify .map() to print '🗺' at the end of each item.
 // Result = 1🗺, 2🗺, 3🗺
 
-const array1 = [1,2,3];
-console.log(array1.map(element => element + '🗺'));
+Array.prototype.map = function () {
+  let array = []
+  this.forEach(element => {
+    array.push(element + '🗺')
+  });
+  return array
+};
+
+console.log([1,2,3].map());
