3691a33121fb553dab4e5fefe23f3281fcaf5f19
Iris D <iris.dijkstra@hotmail.com>
2023-04-21 16:46:26 +0200

    ex.11 jsadv

--- ex10.js
+++ ex10.js
@@ -1,4 +1,9 @@
 // How would you be able to create your own .bind() method using call or apply?
 
-Function.prototype.bind = function(){
-}
+Function.prototype.bind = function(inputFn){
+  if(typeof inputFn === Array) {
+    inputFn.apply(this, this.arguments)
+  } else {
+    inputFn.call(this)
+  };
+};
